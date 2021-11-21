20605a411d697fbd8856b3977afcdc70832ed801
Andrei Errapart <andrei@errapartengineering.com>
2021-11-21 14:28:05 +0100

    Initial import.

---
+++ mkzip.bat
@@ -0,0 +1,5 @@
+rm -rf Nokia12Emulator/bin Nokia12Emulator/obj Nokia12Emulator/Nokia12Emulator.suo Nokia12Emulator/Nokia12Emulator.vjsproj.user
+rm -rf NokiaLibrary/bin NokiaLibrary/obj NokiaLibrary/NokiaLibrary.suo NokiaLibrary/NokiaLibrary.vjsproj.user
+
+zip Nokia12Emulator.zip Nokia12Emulator/*.* Nokia12Emulator/Properties/*.* Nokia12Emulator/src/*.* Nokia12Emulator/doc/*.* NokiaLibrary/*.* NokiaLibrary/Properties/*.* NokiaLibrary/src/*.*
+
